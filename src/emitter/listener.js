495cc65842093922a211ac176df2d0d2dbdb9a5d
Max Baldwin <maxrbaldwin2328@gmail.com>
2019-12-04 16:09:37 -0500

    resolve inquiry and manage them with emitter

--- src/emitter/listener.js
+++ src/emitter/listener.js
@@ -1,33 +1,21 @@
 const emitter = require('@emitter');
+const { log } = require('@logging');
 const getFollowUpEmail = require('@emitter/utils/getFollowUpEmail');
 const getInquiryEmail = require('@emitter/utils/getInquiryEmail');
 const isProduction = require('@utils/isProduction');
+const { saveInquiry, deleteInquiry } = require('@db');
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey('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');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const isPrd = isProduction();
 const myEmail = 'maxrbaldwin2328@gmail.com'
-const momsEmail = isPrd ? 'baldwin1255@comcast.net' : myEmail 
+const momsEmail = isPrd ? 'baldwin1255@comcast.net' : myEmail
 
-emitter.on('sendFollowUp', body => {
-  const { email, name } = body;
-  const reciever = isPrd ? email : myEmail;
-  const followUpEmail = getFollowUpEmail(name);
-  const msg = {
-    to: reciever,
-    from: momsEmail,
-    html: followUpEmail,
-  };
-  
-  try {
-    sgMail.send(msg);
-  } catch (err) {
-    emitter.emit('error', 'Error send follow up');
-  }
-})
-
+// CONTACT EMITTER
 emitter.on('sendInquiry', body => {
+  if (process.env.NODE_ENV === 'test') return;
+
   const msg = {
     to: momsEmail,
     from: myEmail,
@@ -37,11 +25,63 @@ emitter.on('sendInquiry', body => {
   
   try {
     sgMail.send(msg);
+    emitter.emit('sendFollowUp', body)
   } catch (err) {
-    emitter.emit('error', 'Error sending to receive');
+    emitter.emit('error', 'Error sending inquiry');
   }
 })
 
-emitter.on('error', () => {
+emitter.on('sendFollowUp', body => {
+  const { email, name } = body;
+  const reciever = isPrd ? email : myEmail;
+  const followUpEmail = getFollowUpEmail(name);
+  const msg = {
+    to: reciever,
+    from: momsEmail,
+    subject: 'Thanks for reaching out to Dance Magic',
+    html: followUpEmail,
+  };
+  
+  try {
+    sgMail.send(msg);
+    emitter.emit('saveInquiry', body);
+  } catch (err) {
+    emitter.emit('error', 'Error send follow up');
+  }
+})
 
+emitter.on('saveInquiry', async body => {
+  try {
+    await saveInquiry(body);
+  } catch (err) {
+    emitter.emit('error', 'Error saving inquiry');
+  }
+});
+// CONTACT EMITER END
+
+// MANAGE INQUIRIES EMITTER
+emitter.on('manageInquiries', async () => {
+  try {
+    const resolvedInquries = await fetchResolvedInquiries();
+    emitter.emit('deleteOldInquiries', resolvedInquries);
+  } catch (err) {
+    emitter.emit('error', 'Error fetching resolved inquiries');
+  }
+})
+
+emitter.on('deleteOldInquiries', async oldInquiries => {
+  try {
+    for (let index = 0; index < oldInquiries.length; i++) {
+      await deleteInquiry(oldInquiries[index]);
+    }
+  } catch (err) {
+    emitter.emit('error', 'error deleting inquiries');
+  }
+})
+// MANAGE INQUIRIES EMITTER END
+
+// ERROR EMITTER
+emitter.on('error', err => {
+  const logLevel = 'error';
+  log(logLevel, err);
 })
