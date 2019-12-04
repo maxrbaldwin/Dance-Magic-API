534f1613a700c0f2258d1a9fc3b226e81eecd7ae
Max Baldwin <maxrbaldwin2328@gmail.com>
2019-12-04 16:11:19 -0500

    added ref to resolve url

--- src/emitter/utils/getInquiryEmail.js
+++ src/emitter/utils/getInquiryEmail.js
@@ -2,11 +2,13 @@ const url = require('url');
 const isProduction = require('@utils/isProduction');
 const isPrd = isProduction();
 
-const host = process.env.host;
-const deleteHost = isPrd ? host : 'http://localhost:9000';
-const deleteRoute = url.resolve(deleteHost, '/api/inquiries/delete');
+const getDeleteRoute = ref => {
+  const host = process.env.HOST;
+  const deleteHost = isPrd ? host : 'http://localhost:9000';
+  return url.resolve(deleteHost, `/api/inquiries/resolve?ref=${ref}`);
+}
 
-const getInquiryEmail = ({ email, message, phone, name }) => `
+const getInquiryEmail = ({ email, message, phone, name, ref }) => `
   <div>
     <h2>Miss Dawn!</h2>
     <h2>This is the BoBo cloud! You have a new inquiry from your website!</h2>
@@ -16,7 +18,7 @@ const getInquiryEmail = ({ email, message, phone, name }) => `
     <p>Message:</p>
     <p>${message}</p>
     <h2>Please click this link below when you have followed up with the client</h2>
-    <a href=${deleteRoute}>Delete inquiry</a>
+    <a href=${getDeleteRoute(ref)}>Resolve inquiry</a>
   </div>
 `
 
