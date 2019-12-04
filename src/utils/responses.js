67d9f4675c2adfe998e90eee3244cd89fb38a832
Max Baldwin <maxrbaldwin2328@gmail.com>
2019-12-04 16:13:41 -0500

    more middleware response messages

--- src/utils/responses.js
+++ src/utils/responses.js
@@ -14,5 +14,21 @@ module.exports = {
     invalidJSON: {
       code: 4,
       message: 'invalid JSON provided',
+    },
+    noRef: {
+      code: 5,
+      message: 'message no user reference provided',
+    },
+    fetchRefFailed: {
+      code: 6,
+      message: 'failed to fetch by ref',
+    },
+    failedSaveOnResolve: {
+      code: 8,
+      message: 'failed to save after resolving. not resolved',
+    },
+    invalidApiKey: {
+      code: 9,
+      message: 'invalid api key from cron',
     }
   };
