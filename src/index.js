e7ca61b2c25e5ad9bc6e47242b15b561393ea550
Max Baldwin <maxrbaldwin2328@gmail.com>
2019-12-06 10:40:55 -0500

    set env vars on start

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('module-alias/register');
 
 const express = require('express');
 const cors = require('cors');
+const setEnvironment = require('@utils/setEnvironment');
 const { log } = require('@logging');
 
 const app = express();
@@ -12,10 +13,18 @@ app.use(cors());
 // configure all routes
 app.use('/', require('@routes'));
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   const level = 'app';
   const message = `Server started. Listening on port ${PORT}`;
+
+  try {
+    await setEnvironment();
+  } catch (err) {
+    log('error', `error setting env vars: ${err}`);
+  }
   log(level, message);
-});
+}).on('error', err => {
+  log('error', `error starting app: ${err}`);
+})
 // for tests
 module.exports = app;
