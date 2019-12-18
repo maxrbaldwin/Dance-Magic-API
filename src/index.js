4433466e3896c6f13c4c9e5ae434f874cf2ba4d9
Max Baldwin <maxrbaldwin2328@gmail.com>
2019-12-18 12:23:42 -0500

    no more fetch env at runtime

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 require('module-alias/register');
+require('dotenv').config({ path: './env/.env' });
 
 const express = require('express');
 const cors = require('cors');
-const setEnvironment = require('@utils/setEnvironment');
 const { log } = require('@logging');
 
 const app = express();
@@ -20,14 +20,7 @@ app.use('/', require('@routes'));
 app.listen(PORT, async () => {
   const level = 'app';
   const message = `Server started. Listening on port ${PORT}`;
-  try {
-    await setEnvironment();
-  } catch (err) {
-    log('error', `error setting env vars: ${err}`);
-  }
   log(level, message);
-}).on('error', err => {
-  log('error', `error starting app: ${err}`);
 })
 // for tests
 module.exports = app;
