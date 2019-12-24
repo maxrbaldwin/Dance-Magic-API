8cb726f0f919175ae812cd58f45dc1423048c513
Max Baldwin <maxrbaldwin2328@gmail.com>
2019-12-24 11:06:55 -0500

    add decrypt

--- src/utils/setEnvironment.js
+++ src/utils/setEnvironment.js
@@ -1,5 +1,9 @@
 const { fetchEnvironmentVariables } = require('@db');
+const { decrypt } = require('@tools/encrypt');
 const { log } = require('@logging');
+
+const keyring = 'environment';
+const keyName = 'secrets';
 
 const serverEnvVars = [
   'HOST',
@@ -14,12 +18,14 @@ module.exports = () => {
   return new Promise(async (resolve, reject) => {
     try {
       const envVars = await fetchEnvironmentVariables();
-      envVars.forEach(env => {
-        const { key, value } = env;
+      const root = envVars.results;
+      for (let index = 0; index < root.length; index++) {
+        const { key, value } = root[index]
+        const decryptedValue = await decrypt(value, keyring, keyName)
         if (serverEnvVars.indexOf(key) > -1) {
-          process.env[key] = value;
+          process.env[key] = decryptedValue.plaintext.toString();
         }
-      })
+      }
       resolve()
     } catch(err) {
       log('error', `Error setting env vars: ${err}`)
