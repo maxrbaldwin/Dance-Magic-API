ddc34752ac4652a68125bd160e29042168120667
maxrbaldwin <maxrbaldwin2328@gmail.com>
2020-01-18 15:50:13 -0500

    fix uptime check

--- src/routes/health/index.js
+++ src/routes/health/index.js
@@ -2,10 +2,10 @@ const Router = require('express').Router()
 
 const getUptime = () => {
   const seconds = process.uptime()
-  const days = Math.floor(seconds / (3600*24));
-  const hours = Math.floor(seconds / (60*60));
-  const minutes = Math.floor(seconds % (60*60) / 60);
-  const secs = Math.floor(seconds % 60);
+  const days = Math.floor(seconds / 86400)
+  const hours = Math.floor((seconds % 86400) / 3600)
+  const minutes = Math.floor(((seconds % 86400) % 3600) / 60)
+  const secs = Math.floor(((seconds % 86400) % 3600) % 60)
 
   return `${days} : ${hours} : ${minutes} : ${secs}`
 }
