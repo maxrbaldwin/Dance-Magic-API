0fe3fcc186debf2dd49c1de48b2b387da77e1ca6
maxrbaldwin <maxrbaldwin2328@gmail.com>
2020-01-22 11:33:30 -0500

    util for whitelisting domains for cors

---
+++ src/utils/corsOptions.js
@@ -0,0 +1,17 @@
+const whitelist = [
+  'https://dance-magic-259922.appspot.com',
+  'https://dancemagicnj.com',
+  'https://www.dancemagicnj.com',
+];
+
+module.exports = {
+  origin: (origin, callback) => {
+    const isSameOrigin = origin === undefined;
+    const isWhitelisted = whitelist.indexOf(origin) !== -1
+    if (isSameOrigin || isWhitelisted) {
+      callback(null, true)
+    } else {
+      callback(new Error('Cors: Not on whitelist'))
+    }
+  }
+}
