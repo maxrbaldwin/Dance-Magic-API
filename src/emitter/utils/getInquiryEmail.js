81566db019bcd461061d7bc30410b1603ae2e715
maxrbaldwin <maxrbaldwin2328@gmail.com>
2020-02-15 16:36:19 -0500

    nothing to resolve now

--- src/emitter/utils/getInquiryEmail.js
+++ src/emitter/utils/getInquiryEmail.js
@@ -1,21 +1,3 @@
-const url = require('url');
-const isProduction = require('@utils/isProduction');
-const isPrd = isProduction();
-
-const getDeleteRoute = ref => {
-  const protocol = isPrd ? 'https' : 'http';
-  const hostname = isPrd ? 'dance-magic-api-dot-dance-magic-259922.appspot.com' : 'localhost';
-  const pathname = '/api/inquiries/resolve';
-  const query = { ref }
-  return url.format({
-    protocol,
-    hostname,
-    pathname,
-    query,
-    ... !isPrd && { port: 9000 }
-  });
-}
-
 const getInquiryEmail = ({ email, message, phone = 'not provided', name, ref }) => `
   <div>
     <h2>Miss Dawn!</h2>
@@ -25,8 +7,6 @@ const getInquiryEmail = ({ email, message, phone = 'not provided', name, ref }) 
     <p>Phone: ${phone}</p>
     <p>Message:</p>
     <p>${message}</p>
-    <h2>Please click this link below when you have followed up with the client</h2>
-    <a href=${getDeleteRoute(ref)}>Resolve inquiry</a>
   </div>
 `
 
