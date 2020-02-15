81233c1d285d779272e20640ed296e8d4676e5da
maxrbaldwin <maxrbaldwin2328@gmail.com>
2020-02-15 16:35:19 -0500

    remove db. dont need these

--- src/routes/inquiries/resolveContact.js
+++
@@ -1,7 +0,0 @@
-module.exports.resolveContact = contact => {
-  contact.resolved = true;
-  contact.when = Date.now();
-  return contact;
-}
-
-module.exports.getResolveMessage = ({ name, email }) => `The inquiry with ${name} at ${email} has been resolved. This contact will be saved for 30 days and then deleted`;
